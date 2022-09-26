6da73e48c6e103fbe936aeb9abd268e08b55bd42
SonuSangam7 <sonusangamcsk1@gmail.com>
2022-09-26 16:01:42 +0530

    reportgenerated

--- controller/expense.js
+++ controller/expense.js
@@ -1,5 +1,6 @@
 const Expense = require('../models/expense')
 const User = require('../models/user')
+const AWS=require('aws-sdk');
 exports.addExpense = (req,res,next) => {
     const {amount,description,category} = req.body
     if(amount == undefined || amount.length === 0 
@@ -57,3 +58,53 @@ exports.showExpensePremium=(req,res,next)=>{
        res.status(200).json({users,success:true})
       })
     }
+
+
+
+   exports.downloadExpense=async(req,res,next)=>{
+    try{
+const expense=await req.user.getExpenses();
+//console.log(expense);
+const stringifiedExpense=JSON.stringify(expense);
+const userId=req.user.id;
+const filename=`Expense${userId}/${new Date()}.txt`;
+const fileURL= await uploadToS3(stringifiedExpense,filename);
+console.log('>>>>>',fileURL);
+res.status(200).json({fileURL,success:true})
+    }catch(err){
+        res.status(500).json({fileURL:'',success:'false'})
+    }
+   } 
+function uploadToS3(data,filename){
+const BUCKET_NAME='expensetracker07';
+const IAM_USER_KEY='AKIAZNTZYIBRREJDQS44';
+const IAM_USER_SECRET='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'
+
+
+let s3bucket=new AWS.S3({
+    accessKeyId:IAM_USER_KEY,
+    secretAccessKey:IAM_USER_SECRET
+})
+
+    var params={
+        Bucket:BUCKET_NAME,
+        Key:filename,
+        Body:data,
+        ACL:'public-read'
+    }
+    return new Promise((resolve,reject)=>{
+        s3bucket.upload(params,(err,s3response)=>{
+            if(err){
+                //console.log('Something went Wrong',err)
+                reject(err);
+
+            }
+            else{
+                //console.log('success',s3response)
+                resolve( s3response.Location);
+            }
+        })
+    })
+   
+
+}
